29357af499cfed9c81cbc732f4a2f873a0975750
Isabelle <tavaresisaabelle@gmail.com>
2019-08-14 20:27:11 -0300

    GoBarber-second-commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,12 @@
+require('dotenv/config');
+
 module.exports = {
   dialect: 'postgres',
-  host: 'localhost',
-  username: 'postgres',
-  password: 'docker',
-  database: 'gobarber',
+  host: process.eventNames.DB_HOST,
+  username: process.eventNames.DB_USER,
+  password: process.eventNames.DB_PASS,
+  database: process.eventNames.DB_NAME,
+  port: '5432',
   define: {
     timestamps: true,
     underscored: true,
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,21 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import AppointmentController from './app/controllers/AppointmentController';
+import multerConfiig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import NotificationController from './app/controllers/NotificationController';
 
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
+import AvailableController from './app/controllers/AvailableController';
 
 const routes = new Router();
+const upload = multer(multerConfiig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -13,5 +23,19 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/Available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
